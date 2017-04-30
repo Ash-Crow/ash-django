bb409a53dbdf730a58da41298deb4635382d28ca
Ash_Crow <ash@ashtree.eu>
2017-04-30 23:11:51 +0000

    renamed main app

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oauth_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ash-django_app.settings")
 
 app = get_wsgi_application()
