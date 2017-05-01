78e12aaeb11f47f57b9d22a0d94c452d2737c6f5
Sylvain Boissel <sylvain@ashtree.eu>
2017-05-01 12:07:45 +0200

    proper apps

--- ash-django_app/settings.py
+++ ash-django_app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config.get('django', 'django_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'tools.wmflabs.org',
